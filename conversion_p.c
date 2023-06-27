8c21c332fe1d3fd50a779b6f7cb0ebe166906bee
root <RobinLeeHenry101@gmail.com>
2023-06-27 05:57:29 -0700

    conversion p.c file

---
+++ conversion_p.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * handle_p - takes in the args and count for the conversion specifier
+ * 
+ *
+ */
+int handle_p(va_list args, int *count) {
+    void *ptr = va_arg(args, void *);
+
+    int written = printf("%p", ptr);
+    if (written >= 0) 
+    {
+        *count += written;
+        return 0
+    } 
+    else 
+    {
+        return -1
+    }
+}
+
+/**
+ *
+ *
+ */
+int main() 
+{
+    int result = register_printf_function('p', handle_p);
+    if (result == -1) 
+    {
+        fprintf(stderr, "Failed to register conversion specifier '%p'\n");
+        return 1
+    }
+
+    void *ptr = NULL;
+    printf("Pointer address: %p\n", ptr);
+
+    return 0;
+}
